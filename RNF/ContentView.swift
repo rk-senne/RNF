3b05ef9501f726077ebc443d37bba9ba5828b321
kr-senne <rego.senne@icloud.com>
2026-03-13 20:27:14 +0200

    Initial Commit

---
+++ RNF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RNF
+//
+//  Created by Regomoditswe Senne on 2026/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RNF/RNFApp.swift
@@ -0,0 +1,17 @@
+//
+//  RNFApp.swift
+//  RNF
+//
+//  Created by Regomoditswe Senne on 2026/03/13.
+//
+
+import SwiftUI
+
+@main
+struct RNFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
